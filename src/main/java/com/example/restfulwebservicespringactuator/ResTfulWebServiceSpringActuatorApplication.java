01066f45233c2f565f522bb1f279ccf790f00eb5
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2024-09-15 07:12:32 +0200

    initial commit

---
+++ src/main/java/com/example/restfulwebservicespringactuator/ResTfulWebServiceSpringActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulwebservicespringactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulWebServiceSpringActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulWebServiceSpringActuatorApplication.class, args);
+	}
+
+}
